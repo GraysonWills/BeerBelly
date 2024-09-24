25f4e91789a8321e0ce307b616e0942faccf0469
GraysonWills <146792117+GraysonWills@users.noreply.github.com>
2024-09-23 22:42:01 -0400

    Some light modifications to the animations

--- BeerBelly/src/components/InfoCard.js
+++ BeerBelly/src/components/InfoCard.js
@@ -1,25 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/infoCard.css';
 
 const InfoCard = ({ descriptor, data }) => {
-  const [hovered, setHovered] = useState(false);
-
-  // Extract the image and description from the data based on the descriptor
   const description = data[descriptor]?.description || "No description available";
-  const image = data[descriptor]?.image || "/default-image-path.jpg"; // Default image path if not found
+  const image = data[descriptor]?.image || "/default-image-path.jpg";
 
   return (
     <div
-      className={`info-card ${hovered ? 'hovered' : ''}`}
+      className="info-card"
       style={{ backgroundImage: `url(${image})` }}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
     >
-      {hovered && (
-        <div className="info-card-text">
-          {description}
-        </div>
-      )}
+      <div className="info-card-text">
+        {description}
+      </div>
     </div>
   );
 };
